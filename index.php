42110e08f0403bb2566b565583c6b6eceab7b52e
David Cole <156231726+dcole2004@users.noreply.github.com>
2026-03-08 22:22:49 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Symptra Healthcare</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="navbar">
+<h2>Symptra</h2>
+
+<div>
+<a href="login.php">Login</a>
+<a href="register.php">Register</a>
+</div>
+</div>
+
+<div class="container">
+
+<h1>Symptra Healthcare Portal</h1>
+
+<h3>Healthcare Made Simple!</h3>
+
+    <p>
+        Symptra is a modern healthcare portal that allows patients to manage
+        appointments, access medical records, and communicate with providers securely and effectively.
+    </p>
+
+<div class="card">
+<p>Secure patient portal for accessing medical records, appointments and healthcare updates.</p>
+</div>
+
+</div>
+
+
+<footer>
+
+<p>© 2026 Symptra Healthcare Platform</p>
+<p>Edward Forrester, David Cole, Fuhad Bailey, Ali Ansari</p>
+
+
+</footer>
+
+</body>
+</html>
