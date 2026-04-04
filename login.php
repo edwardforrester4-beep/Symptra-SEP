91baf519edfd0ff1666afdfdbedfbf8310ca5d87
David Cole <156231726+dcole2004@users.noreply.github.com>
2026-04-03 23:21:53 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,44 +1,48 @@
 <?php
 session_start();
+include 'db.php';
 
-if(isset($_POST['login'])){
+$error="";
 
+if($_POST){
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-if($username=="patient1" && $password=="123"){
+$res=$conn->query("SELECT * FROM users WHERE username='$username'");
+$user=$res->fetch_assoc();
 
-$_SESSION['role']="patient";
+if($user && password_verify($password,$user['password'])){
+$_SESSION['user_id']=$user['id'];
+$_SESSION['role']=$user['role'];
+
+if($user['role']=="admin"){
+header("Location: admin_dashboard.php");
+}else{
 header("Location: dashboard.php");
-
 }
-
-elseif($username=="admin1" && $password=="123"){
-
-$_SESSION['role']="admin";
-header("Location: admin.php");
-
+}else{
+$error="Invalid Login";
 }
-
-else{
-
-echo "Invalid login";
-
-}
-
 }
 ?>
 
 <link rel="stylesheet" href="style.css">
 
-<h1>Login</h1>
+<div class="container">
+<div class="card">
+
+<h2>Login</h2>
 
 <form method="POST">
-
 <input name="username" placeholder="Username">
-
 <input type="password" name="password" placeholder="Password">
 
-<button name="login">Login</button>
+<p style="color:red;"><?php echo $error; ?></p>
 
+<button>Login</button>
 </form>
+
+<a class="link" href="register.php">New User?</a>
+
+</div>
+</div>
