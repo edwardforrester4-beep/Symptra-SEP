174281fe7d6475eb8faefe33bfb58c391570f04f
David Cole <156231726+dcole2004@users.noreply.github.com>
2026-04-03 23:17:46 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,12 @@
 <?php
+$conn = new mysqli(
+    "sql200.infinityfree.com",
+    "if0_41566043",
+    "Salemhs202126",
+    "if0_41566043_symptra"
+);
 
-$conn = new mysqli("localhost","dcole20","MemorialSalemhs202129","symptra");
-
-if($conn->connect_error){
-die("Database failed");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
