fddbc80708fd6a8f026c3fd0476c8a9c9834cae0
David Cole <156231726+dcole2004@users.noreply.github.com>
2026-04-24 13:43:48 -0400

    Create pharmacies.php

---
+++ pharmacies.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style.css">
+
+<?php include 'navbar.php'; ?>
+
+<div class="container">
+
+<div class="card">
+
+<iframe width="100%" height="400" style="border:0;border-radius:20px;"
+src="https://www.google.com/maps?q=pharmacy+near+me&output=embed"></iframe>
+
+</div>
+
+</div>
